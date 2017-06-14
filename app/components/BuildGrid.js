94da6c4504c422de576945081933e4a6f9e78fdc
Ben Fairbanks <bdfairbanks3@gmail.com>
2017-06-14 13:40:36 -0500

    more DRY, less that the full ammount of profiles being shown on home page for some reason.

--- app/components/BuildGrid.js
+++ app/components/BuildGrid.js
@@ -15,46 +15,41 @@ var Link = require("react-router-dom").Link;
 // is then used to decide the rendering order of the profiles that appear on screen.
 
 function BuildGrid (props) {
-    var profileHolder = [];
-    var numHolder = [];
-    if (props.specialty != "All"){
-      for (var i=0; i<Object.keys(props.profiles).length; i++){
-        if (props.profiles[i][3] == props.specialty){
-          profileHolder.push(props.profiles[i])
-        }
-      }
-      profileHolder.map(function (x){
-        var num = random.integer(0, Object.keys(profileHolder).length-1)
-        if (numHolder.includes(num) == false){
-          numHolder.push(num)
-        } 
-      })
-    }
-    else{
-      for (var i=0; i<Object.keys(props.profiles).length; i++){
+  var profileHolder = [];
+  var numHolder = [];
+  if (props.specialty != "All"){
+    for (var i=0; i<Object.keys(props.profiles).length; i++){
+      if (props.profiles[i][3] == props.specialty){
         profileHolder.push(props.profiles[i])
       }
-       profileHolder.map(function (x){
-        var num = random.integer(0, Object.keys(profileHolder).length-1)
-        if (numHolder.includes(num) == false){
-          numHolder.push(num)
-        }})
     }
-      return(
-      <div className="mainPageProfiles">
-        {numHolder.map(function (x){
-          var fullName = profileHolder[x][1] + profileHolder[x][2]
-          return( 
-            <div key={profileHolder[x][1]} className={profileHolder[x][3]}>          
-              <Link className="link" to={{ pathname: '/profile/'+fullName, query: { query: profileHolder[x] } }}>
-                <img src={profileHolder[x][0]} className="profilePic"/>
-                <h1>{profileHolder[x][1]} {profileHolder[x][2]}</h1>
-                <h1>{profileHolder[x][3]}</h1>
-              </Link>
-            </div>
-          )})}
-     </div>
-    )  
+  }
+  else{
+    for (var i=0; i<Object.keys(props.profiles).length; i++){
+      profileHolder.push(props.profiles[i])
+    }
+  }
+  profileHolder.map(function (x){
+    var num = random.integer(0, Object.keys(profileHolder).length-1)
+    if (numHolder.includes(num) == false){
+      numHolder.push(num)
+    }
+  })
+  return(
+    <div className="mainPageProfiles">
+      {numHolder.map(function (x){
+        var fullName = profileHolder[x][1] + profileHolder[x][2]
+        return( 
+          <div key={profileHolder[x][1]} className={profileHolder[x][3]}>          
+            <Link className="link" to={{ pathname: '/profile/'+fullName, query: { query: profileHolder[x] } }}>
+              <img src={profileHolder[x][0]} className="profilePic"/>
+              <h1>{profileHolder[x][1]} {profileHolder[x][2]}</h1>
+              <h1>{profileHolder[x][3]}</h1>
+            </Link>
+          </div>
+        )})}
+   </div>
+  )  
 } 
 
 module.exports = BuildGrid
